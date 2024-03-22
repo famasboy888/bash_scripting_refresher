4fa10bb58b56f612f472eccf326759150c6c7e06
k-dev1234 <kyleyap.developer@gmail.com>
2024-03-22 09:18:39 +0800

    Adding day 2

---
+++ day-2/day-2.md
@@ -0,0 +1,2 @@
+# Day 2
+- 1) **Get user input**
---
+++ day-2/input.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+#Ask question and take input
+echo "What is your favorite color?"
+read color
+
+echo "How old are you?"
+read age
+
+echo "Your favorite color is $color and you are $age years old"
